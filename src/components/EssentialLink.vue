55f3fd6aef2409ceca9bb966924adfac3237e24b
EvanGrinde <evan.grinde@gmail.com>
2020-10-24 22:18:49 -0400

    allow external links in drawer

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,48 +1,39 @@
 <template>
-  <q-item
-    clickable
-    tag="a"
-    :to="link"
-  >
-    <q-item-section
-      v-if="icon"
-      avatar
-    >
-      <q-icon :name="icon" />
-    </q-item-section>
-
-    <q-item-section>
-      <q-item-label>{{ title }}</q-item-label>
-      <q-item-label caption>
-        {{ caption }}
-      </q-item-label>
-    </q-item-section>
-  </q-item>
+	<q-item v-if="!link.includes('http')" clickable tag="a" :to="link">
+		<q-item-section>
+			<q-item-label>{{ title }}</q-item-label>
+		</q-item-section>
+	</q-item>
+	<q-item v-else clickable tag="a" :href="link" target="_blank">
+		<q-item-section>
+			<q-item-label>{{ title }}</q-item-label>
+		</q-item-section>
+	</q-item>
 </template>
 
 <script>
-export default {
-  name: 'EssentialLink',
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
+	export default {
+		name: "EssentialLink",
+		props: {
+			title: {
+				type: String,
+				required: true,
+			},
 
-    caption: {
-      type: String,
-      default: ''
-    },
+			caption: {
+				type: String,
+				default: "",
+			},
 
-    link: {
-      type: String,
-      default: '#'
-    },
+			link: {
+				type: String,
+				default: "#",
+			},
 
-    icon: {
-      type: String,
-      default: ''
-    }
-  }
-}
+			icon: {
+				type: String,
+				default: "",
+			},
+		},
+	};
 </script>
