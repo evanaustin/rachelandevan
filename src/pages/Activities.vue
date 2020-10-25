adcb9e1f8d9b73f8f92f9c53acab157efe18a11b
EvanGrinde <evan.grinde@gmail.com>
2020-10-25 19:41:31 -0400

    add content for Activities

--- src/pages/Activities.vue
+++ src/pages/Activities.vue
@@ -1,13 +1,71 @@
 <template>
-  <q-page class="flex flex-center">
-    Coming soon &hellip; please check back later!
-  </q-page>
+	<q-page>
+		<h1>Activities</h1>
+		<ul>
+			<li>
+				<a
+					href="https://travel.usnews.com/Key_West_FL/Things_To_Do/Fort_Zachary_Taylor_Historic_State_Park_16750/"
+					target="_blank"
+					>Fort Zachary Taylor Historic State Park</a
+				>
+			</li>
+			<ul>
+				<li>Beach, hike, bike, history</li>
+				<li>$6 entry fee</li>
+			</ul>
+			<li>
+				<a
+					href="https://travel.usnews.com/Key_West_FL/Things_To_Do/Boat_Tours_63130/"
+					target="_blank"
+					>Key West Boat Tours</a
+				>
+			</li>
+			<ul>
+				<li>Sailing, snorkeling, private charters</li>
+				<li>Prices vary by tour company</li>
+			</ul>
+			<li>
+				<a
+					href="https://travel.usnews.com/Key_West_FL/Things_To_Do/Key_West_Butterfly_and_Nature_Conservatory_16764/"
+					target="_blank"
+					>Key West Butterfly and Nature Conservancy</a
+				>
+			</li>
+			<ul>
+				<li>Butterflies everywhere!</li>
+				<li>$12 per person</li>
+			</ul>
+			<li>
+				<a
+					href="https://travel.usnews.com/Key_West_FL/Things_To_Do/Southernmost_Point_16758/"
+					target="_blank"
+					>Southernmost Point</a
+				>
+			</li>
+			<ul>
+				<li>
+					Snap a pic with a colorful buoy marking the southernmost point of the
+					US
+				</li>
+				<li>Free</li>
+			</ul>
+			<li>
+				<a href="https://www.nps.gov/drto/index.htm" target="_blank"
+					>Dry Tortugas National Park</a
+				>
+			</li>
+			<ul>
+				<li>Fort Jackson, swimming, snorkeling, camping, boating</li>
+				<li>$15 per person (includes ferry fee)</li>
+			</ul>
+		</ul>
+	</q-page>
 </template>
 
 <script>
-export default {
-  name: 'Our Story'
-}
+	export default {
+		name: "Our Story",
+	};
 </script>
 
 <style lang="scss">
