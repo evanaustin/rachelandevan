2ee0871464f4d11f94f42725604f3601031d4319
Evan Grinde <evan.grinde@gmail.com>
2021-06-29 20:29:59 -0400

    update after party address

--- src/pages/AfterParty.vue
+++ src/pages/AfterParty.vue
@@ -1,72 +1,77 @@
 <template>
-	<q-page>
-		<div class="q-pa-md full-width">
-			<h1>After Party</h1>
-			<h5>Saturday July 17, 2021: Our Happily Ever After Party!</h5>
-      <p>After our elopment in May, we'll have a gathering for family and friends in July.</p>
-			<ul>
-				<li>
-					<strong
-						>After Party at
-						<a href="https://hiddenvalleycc.com" target="_blank"
-							>Hidden Valley Country Club</a
-						></strong
-					>
-				</li>
-				<ul>
-					<li>2500 Romar Road</li>
-					<li>Salem, VA 24153</li>
-					<li>7:00p - 11:00p</li>
-					<li>Attire: Dressy Casual</li>
-					<li>Hors d'oeuvres will be served</li>
-				</ul>
-			</ul>
-			<h5>Accomodations</h5>
-			<ul>
-				<li>
-					If traveling from out of town, there are several options in and around
-					Roanoke & Salem! We don’t have any hotel blocks, but here are some
-					options that we like (and feel free to find something else you like!):
-				</li>
-				<ul>
-					<li>
-						<a href="https://theloftsatdowntownsalem.com" target="_blank"
-							>The Lofts at Downtown Salem</a
-						>
-						- approx. 10 mins from HVCC
-					</li>
-					<li>
-						<a
-							href="https://www.marriott.com/hotels/travel/roano-fairfield-inn-and-suites-roanoke-hollins-i-81/"
-							target="_blank"
-							>Fairfield Inn & Suites</a
-						>
-						- approx. 10 mins from HVCC
-					</li>
-					<li>
-						<a href="https://www.roanokeboutiquehotel.com" target="_blank"
-							>Roanoke Boutique Hotel</a
-						>
-						- approx. 15 mins from HVCC
-					</li>
-					<li>
-						<a
-							href="https://www.hilton.com/en/book/reservation/rooms/?ctyhocn=ROACUQQ&arrivalDate=2021-07-17&departureDate=2021-07-18&viewPackagesAndPromotionsRate=true"
-							target="_blank"
-							>Hotel Roanoke</a
-						>
-						- approx. 20 mins from HVCC
-					</li>
-				</ul>
-			</ul>
-		</div>
-	</q-page>
+  <q-page>
+    <div class="q-pa-md full-width">
+      <h1>After Party</h1>
+      <h5>Saturday July 17, 2021: Our Happily Ever After Party!</h5>
+      <p>
+        After our elopment in May, we'll have a gathering for family and friends
+        in July.
+      </p>
+      <ul>
+        <li>
+          <strong
+            >After Party at
+            <a
+              href="https://www.google.com/maps/place/1700+Bridgewater+Bay+Dr,+Moneta,+VA+24121/@37.1496098,-79.6551623,17z/data=!3m1!4b1!4m5!3m4!1s0x884d40d30a95de13:0x1e16e8500b5c7616!8m2!3d37.1496055!4d-79.6529736"
+              target="_blank"
+              >Bridgewater Bay Clubhouse</a
+            ></strong
+          >
+        </li>
+        <ul>
+          <li>1700 Bridgewater Bay Dr</li>
+          <li>Moneta, VA 24121</li>
+          <li>7:00p - 11:00p</li>
+          <li>Attire: Dressy Casual</li>
+          <li>Hors d'oeuvres will be served</li>
+        </ul>
+      </ul>
+      <h5>Accomodations</h5>
+      <ul>
+        <li>
+          If traveling from out of town, there are several options in and around
+          Roanoke & Salem! We don’t have any hotel blocks, but here are some
+          options that we like (and feel free to find something else you like!):
+        </li>
+        <ul>
+          <li>
+            <a href="https://theloftsatdowntownsalem.com" target="_blank"
+              >The Lofts at Downtown Salem</a
+            >
+            <!-- - approx. 10 mins from HVCC -->
+          </li>
+          <li>
+            <a
+              href="https://www.marriott.com/hotels/travel/roano-fairfield-inn-and-suites-roanoke-hollins-i-81/"
+              target="_blank"
+              >Fairfield Inn & Suites</a
+            >
+            <!-- - approx. 10 mins from HVCC -->
+          </li>
+          <li>
+            <a href="https://www.roanokeboutiquehotel.com" target="_blank"
+              >Roanoke Boutique Hotel</a
+            >
+            <!-- - approx. 15 mins from HVCC -->
+          </li>
+          <li>
+            <a
+              href="https://www.hilton.com/en/book/reservation/rooms/?ctyhocn=ROACUQQ&arrivalDate=2021-07-17&departureDate=2021-07-18&viewPackagesAndPromotionsRate=true"
+              target="_blank"
+              >Hotel Roanoke</a
+            >
+            <!-- - approx. 20 mins from HVCC -->
+          </li>
+        </ul>
+      </ul>
+    </div>
+  </q-page>
 </template>
 
 <script>
-	export default {
-		name: "AfterParty",
-	};
+export default {
+  name: "AfterParty",
+};
 </script>
 
 <style lang="scss">
